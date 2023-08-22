d44f863c81240d5934c3662797481d39bcea2883
SaniyaSalariaBedi <iinderjitbedi@gmail.com>
2023-08-21 22:16:01 -0400

    CSS fixes

--- src/features/auth/pages/orgAdmin/orgRegister.js
+++ src/features/auth/pages/orgAdmin/orgRegister.js
@@ -24,7 +24,7 @@ export default function RegisterOrganization() {
                     <div className='sidebar'>
                         <div className='content-box'>
                             <div className='logo'>Wagmatcook</div>
-                            <h4 className="mt-6">Communicate to Employees via Feeds, SMS, Email</h4>
+                            <h4 className="mt-3">Communicate to Employees via Feeds, SMS, Email</h4>
                             <p>Welcome to our website! We hope it provides you with a good perspective about our community. Better yet, come visit us and the experiences we are willing to share with you.</p>
                         </div>
                         <div className=''>
@@ -39,47 +39,51 @@ export default function RegisterOrganization() {
                         </h1>
                         <p className='text'>Please provide your Organization information</p>
                         <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                            <div>
+                                <div class="file-input flex items-center ">
 
-                            <div class="file-input flex items-center  mb-1">
-
-                                <div class="h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-                                    <div className='image-container '  >
-                                        <img src="../assets/user.png"  alt='user' />
-                                    </div>
-                                    {/* <div class="h-12 w-12 rounded-full overflow-hidden image-container">
+                                    <div class="h-12 w-12 rounded-full overflow-hidden bg-gray-100">
+                                        <div className='image-container '  >
+                                            <img src="../assets/user.png" alt='user' />
+                                        </div>
+                                        {/* <div class="h-12 w-12 rounded-full overflow-hidden image-container">
                                         <img src="../assets/user.png" alt="" class="" />
                                     </div> */}
-                                </div>
-
-                                <div class="flex items-center">
-                                    <div class="ml-5 rounded-md shadow-sm">
-                                        <input
-                                            type="file"
-                                            accept="image/*,capture=camera"
-                                            name="photo" id="photo"
-                                            className="custom" />
-                                        <label className='org-logo'>Organization Logo</label>
-                                        <label for="photo"
-                                            class="upload-photo">
-
-                                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M2 10C2 11.8856 2 12.8284 2.58579 13.4142C3.17157 14 4.11438 14 6 14H10C11.8856 14 12.8284 14 13.4142 13.4142C14 12.8284 14 11.8856 14 10" stroke="#595959" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                                <path d="M7.99967 10.6667V2M7.99967 2L10.6663 4.91667M7.99967 2L5.33301 4.91667" stroke="#595959" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                            </svg> &nbsp;
-
-                                            Upload Photo
-                                        </label>
                                     </div>
-                                    <button
-                                        type="button"
-                                        aria-label="Remove button"
-                                        class="mx-1 mt-2 remove-button">
-                                        Remove
-                                    </button>
+
+                                    <div class="flex items-center">
+                                        <div class="ml-5 rounded-md">
+                                            <input
+                                                type="file"
+                                                accept="image/*,capture=camera"
+                                                name="photo" id="photo"
+                                                className="custom" />
+                                            <label className='org-logo'>Organization Logo</label>
+                                            <label for="photo"
+                                                class="upload-photo">
+
+                                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M2 10C2 11.8856 2 12.8284 2.58579 13.4142C3.17157 14 4.11438 14 6 14H10C11.8856 14 12.8284 14 13.4142 13.4142C14 12.8284 14 11.8856 14 10" stroke="#595959" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                                    <path d="M7.99967 10.6667V2M7.99967 2L10.6663 4.91667M7.99967 2L5.33301 4.91667" stroke="#595959" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                                </svg> &nbsp;
+
+                                                Upload Photo
+                                            </label>
+                                        </div>
+
+                                        <button
+                                            type="button"
+                                            aria-label="Remove button"
+                                            class="mx-1 mt-2 remove-button">
+                                            Remove
+                                        </button>
+                                        {/* <span className='gray-text'>*png *jpeg up to 10MB at least 400px by 400px</span> */}
+                                    </div>
+
+
                                 </div>
-
+                                <span className='upload-info'>*png *jpeg up to 10MB at least 400px by 400px</span>
                             </div>
-
                             <InputLabel>Organization name</InputLabel>
                             <TextField
                                 margin="normal"
--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { Route, Navigate, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+import {
+  Route,
+  Navigate,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import routes from "./routes";
 // import NotFound from "../features/auth/pages/notFound";
 import PublicLayout from "../layouts/PublicLayout";
@@ -11,55 +17,59 @@ import ScrollToTop from "../features/auth/pages/scrollTop";
 // import IntegolfLoader from '../shared/integolf-loader/integolf-loader';
 
 const Navigation = () => {
-  const isUser: boolean = !!JSON.parse(localStorage.getItem('user') || "");//useAppSelector(isUserLoggedIn);
+  const isUser: boolean = !!localStorage.getItem("user")
+    ? JSON.parse(localStorage.getItem("user") || "")
+    : ""; //useAppSelector(isUserLoggedIn);
   // const showSpinner = useAppSelector(fetchAccountStorageSpinnerState);
   console.log(isUser);
-
-
 
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
         <Route path="/landing.html" />
 
-        {routes.map(({ component: Component, path, type, to, title, children }) => (
-
-          <React.Fragment key={Date.now()} >
-            <Route
-              path={path}
-              key={path}
-              element={
-                <ScrollToTop>
-                  {(type === 'public' && !isUser )|| type === 'semi' ? (
-                    <PublicLayout component={Component} />
-                  ) : type === 'private' && isUser ? (
-                    <PrivateLayout component={Component}>
-                      {children &&
-                        children.length &&
-                        children.map((e: any) => (
-                          <Route path={e.path} key={e.path} element={<e.component />} />
-                        ))}
-                    </PrivateLayout>
-                  ) : (
-                    <Navigate replace to={to} />
-                  )}
-                </ScrollToTop>
-              }
-            />
-
-          </React.Fragment >
-        ))
-        }
+        {routes.map(
+          ({ component: Component, path, type, to, title, children }) => (
+            <React.Fragment key={Date.now()}>
+              <Route
+                path={path}
+                key={path}
+                element={
+                  <ScrollToTop>
+                    {(type === "public" && !isUser) || type === "semi" ? (
+                      <PublicLayout component={Component} />
+                    ) : type === "private" && isUser ? (
+                      <PrivateLayout component={Component}>
+                        {children &&
+                          children.length &&
+                          children.map((e: any) => (
+                            <Route
+                              path={e.path}
+                              key={e.path}
+                              element={<e.component />}
+                            />
+                          ))}
+                      </PrivateLayout>
+                    ) : (
+                      <Navigate replace to={to} />
+                    )}
+                  </ScrollToTop>
+                }
+              />
+            </React.Fragment>
+          )
+        )}
         {/* <Route path="*" element={<NotFound />} /> */}
       </>
     )
   );
 
-  return <>
-    <RouterProvider router={router} />
-    {/* <IntegolfLoader show={showSpinner}/> */}
-  </>
-
+  return (
+    <>
+      <RouterProvider router={router} />
+      {/* <IntegolfLoader show={showSpinner}/> */}
+    </>
+  );
 };
 
 export default Navigation;
