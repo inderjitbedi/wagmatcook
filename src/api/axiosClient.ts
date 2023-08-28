6161dac854cd3e0cc8105f85fc8729e77ef2af41
Inderjit Bedi <iinderjitbedi@gmail.com>
2023-08-28 10:10:06 -0400

    New FE build.

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,7 +1,8 @@
 import axios, {  AxiosResponse } from "axios";
 
 const axiosClient =  axios.create({
-  baseURL: 'http://localhost:4041/api',//`${process.env.REACT_APP_USER_API_URL}`,
+  // baseURL: 'http://localhost:4041/api',//`${process.env.REACT_APP_USER_API_URL}`,
+  baseURL: 'http://hrapp.chantsit.com/api',//`${process.env.REACT_APP_USER_API_URL}`,
   headers: {
     'Content-Type': 'application/json'
   },
