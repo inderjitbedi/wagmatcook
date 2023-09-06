031dec1a4776c5d5bff753dcf23493a6db13898d
Lalit kumar <lalit.kumar@quantumsystem.in>
2023-09-06 09:27:16 +0530

    just a morning commit to take pull

---
+++ src/Employee/ViewEmployee/EmployeeJobDetails.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EmployeeJobDetails = () => {
+  return (
+    <div>EmployeeJobDetails</div>
+  )
+}
+
+export default EmployeeJobDetails
---
+++ src/Employee/ViewEmployee/EmployeePersonal.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EmployeePersonal = () => {
+  return (
+    <div>EmployeePersonal</div>
+  )
+}
+
+export default EmployeePersonal
