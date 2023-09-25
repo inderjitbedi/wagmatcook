c52c65099c94264984bd33da52824f02d1ef4050
SaniyaSalariaBedi <iinderjitbedi@gmail.com>
2023-09-25 12:46:01 -0400

    Update leaveType.js

--- api/routes/leaveType.js
+++ api/routes/leaveType.js
@@ -7,7 +7,7 @@ router.post('/create', verifyToken, leaveTypeController.create);
 router.put('/update/:id', verifyToken, leaveTypeController.update);
 router.put('/delete/:id', verifyToken, leaveTypeController.delete);
 router.get('/list', verifyToken, leaveTypeController.list); // /list?page=1&limit=10&searchKey=search_keyword
-router.get('/emplyee-list/:employeeid', verifyToken, leaveTypeController.employeeLeaveTypeList); // /list?page=1&limit=10&searchKey=search_keyword
+router.get('/employee-list/:employeeid', verifyToken, leaveTypeController.employeeLeaveTypeList); // /list?page=1&limit=10&searchKey=search_keyword
 router.get('/detail/:id', verifyToken, leaveTypeController.detail);
 router.put('/reorder', verifyToken, leaveTypeController.reorder);  // disciplinaries: [id,id,id]
 
