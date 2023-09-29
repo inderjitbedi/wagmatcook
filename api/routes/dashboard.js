1f626393f0711c7781e08ca3b8edad5fa4a435cb
SaniyaSalariaBedi <iinderjitbedi@gmail.com>
2023-09-28 22:16:20 -0400

    Update dashboard.js

--- api/routes/dashboard.js
+++ api/routes/dashboard.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const employeeController = require('../controllers/employee');
 const departmentController = require('../controllers/department');
 const { verifyToken } = require('../middlewares/jwtMiddleware');
+const roles = require('../enum/roles');
 
-router.get('/employee/list', verifyToken, employeeController.dashboardList);
-router.get('/department/list', verifyToken, departmentController.dashboardList); // /list?page=1&limit=10&searchKey=search_keyword
+router.get('/employee/list', verifyToken([roles.ORG_ADMIN]), employeeController.dashboardList);
+router.get('/department/list', verifyToken([roles.ORG_ADMIN]), departmentController.dashboardList); // /list?page=1&limit=10&searchKey=search_keyword
 
 
 module.exports = router;
