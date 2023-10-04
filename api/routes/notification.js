0db8be8c8448b89e6480d1ad33f6a4263cf4ce9a
SaniyaSalariaBedi <iinderjitbedi@gmail.com>
2023-10-04 08:19:50 -0400

    OA Permissions

--- api/routes/notification.js
+++ api/routes/notification.js
@@ -4,9 +4,9 @@ const { verifyToken } = require('../middlewares/jwtMiddleware');
 const roles = require('../enum/roles');
 const notificationController = require('../controllers/notification');
 
-router.get('/list', verifyToken([roles.HR, roles.MANAGER, roles.EMPLOYEE]), notificationController.list);
-router.put('/mark-read', verifyToken([roles.HR, roles.MANAGER, roles.EMPLOYEE]), notificationController.markRead);
-router.put('/read-all', verifyToken([roles.HR, roles.MANAGER, roles.EMPLOYEE]), notificationController.markRead);
-router.get('/unread-count', verifyToken([roles.HR, roles.MANAGER, roles.EMPLOYEE]), notificationController.unreadCount);
+router.get('/list', verifyToken([roles.ORG_ADMIN, roles.HR, roles.MANAGER, roles.EMPLOYEE]), notificationController.list);
+router.put('/mark-read', verifyToken([roles.ORG_ADMIN, roles.HR, roles.MANAGER, roles.EMPLOYEE]), notificationController.markRead);
+router.put('/read-all', verifyToken([roles.ORG_ADMIN, roles.HR, roles.MANAGER, roles.EMPLOYEE]), notificationController.markRead);
+router.get('/unread-count', verifyToken([roles.ORG_ADMIN, roles.HR, roles.MANAGER, roles.EMPLOYEE]), notificationController.unreadCount);
 
 module.exports = router;
