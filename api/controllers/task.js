3de135980af88577582fac640eec89f9ce1ba154
SaniyaSalariaBedi <iinderjitbedi@gmail.com>
2023-10-13 09:40:59 -0400

    Task Comments

--- api/controllers/task.js
+++ api/controllers/task.js
@@ -3,6 +3,7 @@ const Notifications = require("../models/notification");
 const Tasks = require("../models/tasks");
 const roles = require("../enum/roles");
 const User = require("../models/user");
+const TaskComments = require("../models/taskComments");
 
 const taskController = {
     async create(req, res) {
@@ -162,7 +163,79 @@ const taskController = {
         }
     },
 
+    async addComment(req, res) {
 
+        try {
+            req.body.task = req.params?.taskid;
+            req.body.commenter = req.user?._id;
+            const comment = new TaskComments(req.body);
+            await comment.save();
+            // let type = "TASK_ASSIGNED"
+            // const notification = new Notifications({
+            //     title: notificationConstants[type].title?.replace('{assigner}', req.user?.personalInfo ? [req.user?.personalInfo.firstName, req.user?.personalInfo.lastName].join(' ') : 'Someone'),
+            //     type,
+            //     sender: req.user._id,
+            //     receiver: req.body.assignee
+            // });
+            // await notification.save();
+            res.status(201).json({ comment, message: 'Comment added successfully.' });
+        } catch (error) {
+            console.error("taskController:create:error -", error);
+            res.status(400).json(error);
+        }
+    },
+    async deleteComment(req, res) {
+        try {
+            const comment = await TaskComments.findOneAndUpdate({
+                _id: req.params.id,
+            }, { isDeleted: true }, { new: true })
+            res.status(200).json({ task, message: 'Comment deleted successfully' });
+        } catch (error) {
+            console.error("taskController:update:error -", error);
+            res.status(400).json(error);
+        }
+    },
+    async updateComment(req, res) {
+        try {
+            req.body.commenter = req.user?._id;
+            req.body.task = req.params?.taskid;
+            req.body.isEdited = true;
+            const task = await TaskComments.findOneAndUpdate({
+                _id: req.params.id,
+            }, { ...req.body }, { new: true })
+            res.status(200).json({ task, message: 'Comment updated successfully' });
+        } catch (error) {
+            console.error("taskController:update:error -", error);
+            res.status(400).json(error);
+        }
+    },
+    async listComments(req, res) {
+        try {
+            const page = parseInt(req.query.page) || 1;
+            const limit = parseInt(req.query.limit) || 10;
+            const startIndex = (page - 1) * limit;
+            let filters = {
+                task: req.params.taskid,
+            }
+            const comments = await TaskComments.find(filters).populate({ path: 'commenter', populate: { path: 'personalInfo', populate: { path: 'photo' } } },).skip(startIndex)
+                .limit(limit)
+                .sort({ createdAt: -1 });
+
+            const totalComments = await TaskComments.countDocuments(filters);
+            const totalPages = Math.ceil(totalComments / req.query.limit);
+
+            res.status(200).json({
+                comments,
+                totalComments,
+                currentPage: page,
+                totalPages,
+                message: 'Comments fetched successfully'
+            });
+        } catch (error) {
+            console.error("taskController:update:error -", error);
+            res.status(400).json(error);
+        }
+    },
 
 }
 module.exports = taskController
